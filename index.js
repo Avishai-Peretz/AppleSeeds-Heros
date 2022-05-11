9df3c065b594915e6a27dd258391edec9f72da6f
benrotlevy <benjamin@rotlevy.com>
2022-05-11 12:59:25 +0300

    table

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
-async function getData() {
-    const prom = await fetch("https://capsules-asb6.herokuapp.com/api/teacher/mordi");
-    const data = await prom.json();
+const allStudents = [];
+
+async function initialize() {
+    const promMordi = await fetch("https://capsules-asb6.herokuapp.com/api/teacher/mordi");
+    const promToam = await fetch("https://capsules-asb6.herokuapp.com/api/teacher/toam");
+    const mordiData = await promMordi.json();
+    const toamData = await promToam.json();
+    const data = mordiData.concat(toamData);
     console.log(data);
+    for(let student of data) {
+        const prom = await fetch(`https://capsules-asb6.herokuapp.com/api/user/${student.id}`);
+        const fullData = await prom.json();
+        allStudents.push(fullData);
+    }
+    console.log(allStudents);
+    createTable();
 }
 
-getData();
+initialize();
+
+function createTable() {
+    const tableContainer = document.querySelector(".table-container")
+    let output = `
+        <table>
+            <tr>
+            <th colspan=5>Students</th>
+            </tr>
+            <tr>
+                <th>id</th>
+                <th>first name</th>
+                <th>last name</th>
+                <th>gender</th>
+                <th>age</th>
+                <th>capsule</th>
+                <th>city</th>
+                <th>hobby</th>
+                <th></th>
+                <th></th>
+            </tr>`;
+    for(let student of allStudents) {
+        output += `
+        <tr>
+            <td>${student.id}</td>
+            <td>${student.firstName}</td>
+            <td>${student.lastName}</td>
+            <td>${student.gender}</td>
+            <td>${student.age}</td>
+            <td>${student.capsule}</td>
+            <td>${student.city}</td>
+            <td>${student.hobby}</td>
+            <td><button id=edit>Edit</button></td>
+            <td><button id=delete>Delete</button></td>
+        </tr>`;
+    }
+    output += `</table>`;
+    tableContainer.innerHTML = output;
+}
