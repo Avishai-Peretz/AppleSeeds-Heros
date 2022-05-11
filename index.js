d83d481125cf9c1f4a5a7055c1ae4a9d7adc57bc
shhady <shhadyse@gmail.com>
2022-05-11 16:35:58 +0300

    SoringButtons

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ async function initialize() {
   const toamData = proms[1].json();
   const results = await Promise.all([mordiData, toamData]);
   const data = results[0].concat(results[1]);
-//   console.log(data);
+  //   console.log(data);
   const allProms = [];
   for (let student of data) {
     const prom = fetch(
@@ -22,7 +22,7 @@ async function initialize() {
   }
   const allPromsResult = await Promise.all(allProms);
   const arrJasons = [];
-  for(let res of allPromsResult) {
+  for (let res of allPromsResult) {
     const fullData = res.json();
     arrJasons.push(fullData);
   }
@@ -37,20 +37,21 @@ initialize();
 
 function createTable() {
   const tableContainer = document.querySelector(".table-container");
+  tableContainer.innerHTML = "";
   let output = `
         <table>
             <tr>
             <th colspan=10 class="titleStudents">Students</th>
             </tr>
             <tr>
-                <th>id</th>
-                <th>first name</th>
-                <th>last name</th>
-                <th>gender</th>
-                <th>age</th>
-                <th>capsule</th>
-                <th>city</th>
-                <th>hobby</th>
+                <th><button id=idSort>id</button></th>
+                <th><button id=firstSort>first name</button></th>
+                <th><button id=lastSort>last name</button></th>
+                <th><button id=genderSort>gender</button></th>
+                <th><button id=ageSort>age</button></th>
+                <th><button id=capsuleSort>capsule</button></th>
+                <th><button id=citySort>city</button></th>
+                <th><button id=hobbySort>hobby</button></th>
                 <th>Edit</th>
                 <th>Delete</th>
             </tr>`;
@@ -73,18 +74,58 @@ function createTable() {
   tableContainer.innerHTML = output;
   const deleteBtn = document.querySelectorAll(".delete");
   addEventToButtons(deleteBtn);
+
+  const ageButton = document.querySelector("#ageSort");
+  ageButton.addEventListener("click", function () {
+    sortTable("age");
+    createTable();
+  });
+  const capsuleButton = document.querySelector("#capsuleSort");
+  capsuleButton.addEventListener("click", function () {
+    sortTable("capsule");
+    createTable();
+  });
+
+  const idButton = document.querySelector("#idSort");
+  idButton.addEventListener("click", function () {
+    sortTable("id");
+    createTable();
+  });
+  const genderButton = document.querySelector("#genderSort");
+  genderButton.addEventListener("click", function () {
+    sortNames("gender");
+    createTable();
+  });
+  const firstButton = document.querySelector("#firstSort");
+  firstButton.addEventListener("click", function () {
+    sortNames("first");
+    createTable();
+  });
+  const lastButton = document.querySelector("#lastSort");
+  lastButton.addEventListener("click", function () {
+    sortNames("last");
+    createTable();
+  });
+  const hobbyButton = document.querySelector("#hobbySort");
+  hobbyButton.addEventListener("click", function () {
+    sortNames("hobby");
+    createTable();
+  });
+  const cityButton = document.querySelector("#citySort");
+  cityButton.addEventListener("click", function () {
+    sortNames("city");
+    createTable();
+  });
 }
 
 function addEventToButtons(buttons) {
   console.log(buttons);
   buttons.forEach((button) => {
     button.addEventListener("click", removePerson);
-    console.log(3);
   });
 }
 
 function removePerson(event) {
-  console.log(2);
   const row = document.querySelector(
     `#row${event.target.getAttribute("data-number")}`
   );
@@ -94,4 +135,20 @@ function removePerson(event) {
 function deleteSpinner() {
   const loading = document.querySelector(".loader");
   loading.remove();
+}
+
+function sortTable(field) {
+  allStudents.sort((student1, student2) => {
+    return student1[field] - student2[field];
+  });
+}
+
+function sortNames(field) {
+  allStudents.sort((student1, student2) => {
+    if (student1[field] > student2[field]) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
 }
