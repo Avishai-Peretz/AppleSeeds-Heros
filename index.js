cb9335ba55bf3f7db652474726a75e8a5277677f
shhady <shhadyse@gmail.com>
2022-05-11 13:47:06 +0300

    styling

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 const allStudents = [];
 
 async function initialize() {
-    const promMordi = await fetch("https://capsules-asb6.herokuapp.com/api/teacher/mordi");
-    const promToam = await fetch("https://capsules-asb6.herokuapp.com/api/teacher/toam");
-    const mordiData = await promMordi.json();
-    const toamData = await promToam.json();
-    const data = mordiData.concat(toamData);
-    console.log(data);
-    for(let student of data) {
-        const prom = await fetch(`https://capsules-asb6.herokuapp.com/api/user/${student.id}`);
-        const fullData = await prom.json();
-        allStudents.push(fullData);
-    }
-    console.log(allStudents);
-    createTable();
+  const promMordi = await fetch(
+    "https://capsules-asb6.herokuapp.com/api/teacher/mordi"
+  );
+  const promToam = await fetch(
+    "https://capsules-asb6.herokuapp.com/api/teacher/toam"
+  );
+  const mordiData = await promMordi.json();
+  const toamData = await promToam.json();
+  const data = mordiData.concat(toamData);
+  console.log(data);
+  for (let student of data) {
+    const prom = await fetch(
+      `https://capsules-asb6.herokuapp.com/api/user/${student.id}`
+    );
+    const fullData = await prom.json();
+    allStudents.push(fullData);
+  }
+  console.log(allStudents);
+  createTable();
 }
 
 initialize();
 
 function createTable() {
-    const tableContainer = document.querySelector(".table-container")
-    let output = `
+  const tableContainer = document.querySelector(".table-container");
+  let output = `
         <table>
             <tr>
-            <th colspan=5>Students</th>
+            <th colspan=10 class="titleStudents">Students</th>
             </tr>
             <tr>
                 <th>id</th>
@@ -34,11 +40,11 @@ function createTable() {
                 <th>capsule</th>
                 <th>city</th>
                 <th>hobby</th>
-                <th></th>
-                <th></th>
+                <th>Edit</th>
+                <th>Delete</th>
             </tr>`;
-    for(let student of allStudents) {
-        output += `
+  for (let student of allStudents) {
+    output += `
         <tr>
             <td>${student.id}</td>
             <td>${student.firstName}</td>
@@ -51,7 +57,7 @@ function createTable() {
             <td><button id=edit>Edit</button></td>
             <td><button id=delete>Delete</button></td>
         </tr>`;
-    }
-    output += `</table>`;
-    tableContainer.innerHTML = output;
+  }
+  output += `</table>`;
+  tableContainer.innerHTML = output;
 }
