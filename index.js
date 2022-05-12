953f54a3372db3342c1e722b5c66a126f152c3ac
Avishai-Peretz <Avishai651@gmail.com>
2022-05-12 03:27:33 +0300

    weather Finals

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ async function initialize() {
   const toamData = proms[1].json();
   const results = await Promise.all([mordiData, toamData]);
   const data = results[0].concat(results[1]);
-  //   console.log(data);
   const allProms = [];
   for (let student of data) {
     const prom = fetch(
@@ -30,7 +29,6 @@ async function initialize() {
   const arrJasonsResults = await Promise.all(arrJasons);
   allStudents = [...arrJasonsResults];
   originalStudents = [...allStudents];
-  //   console.log(allStudents);
   deleteSpinner();
   addEventToSearch();
   addEventToRestart();
@@ -269,27 +267,65 @@ function sortNames(field) {
     }
   });
 }
+
+function getWeatherWindow(event,city, currentWeather, currentPlace) {
+  if (!document.querySelector('.weather_window')) {
+    const weatherWindow = document.createElement('div');
+          weatherWindow.setAttribute('class', 'weather_window');
+    city.appendChild(weatherWindow);
+    const degreesCelsius = document.createElement('div');
+          degreesCelsius.setAttribute('class', 'degrees-celsius');
+          degreesCelsius.innerText = `${currentWeather.temp_c} °C`;
+    const location = document.createElement('div');
+          location.setAttribute('class', 'location');
+          location.innerText = `${event.target.innerText}`;
+    const localTime = document.createElement('div');
+          localTime.setAttribute('class', 'local-time');
+    localTime.innerText = `${currentPlace.localtime}`;
+    weatherWindow.appendChild(degreesCelsius);
+    weatherWindow.appendChild(location);
+    weatherWindow.appendChild(localTime);
+  } else {
+    const removeWeatherWindow = document.querySelector('.weather_window')
+    removeWeatherWindow.parentElement.removeChild(removeWeatherWindow);
+  }
+}
+function unableToGetWeather(event, city) {
+
+    const weatherWindow = document.createElement('div');
+          weatherWindow.setAttribute('class', 'weather_window');
+    city.appendChild(weatherWindow);
+    const location = document.createElement('div');
+          location.setAttribute('class', 'invalid-location');
+          location.innerText = `There is no available weather data for ${event.target.innerText}, please edit to nearby location in english words!`;
+    weatherWindow.appendChild(location)
+  
+}
+
 function getCityForWeather() {
   const studentCityMain = [...document.querySelectorAll(".studentCity")];
-  console.log(studentCityMain);
   studentCityMain.forEach((city) => {
-    city.addEventListener("click", async (event) => {
+    city.addEventListener("mouseover", async (event) => {
       try {
         const weather1 = await fetch(
           `https://api.weatherapi.com/v1/current.json?key=039d505d574143e89f6215143221105&q=${event.target.innerText}, israel&aqi=no`
-          // `http://api.weatherstack.com/current?access_key=c6a38e03037a0aedff539da5dc7f6827&query=${event.target.innerText}`
         );
         const data = await weather1.json();
         const currentWeather = data.current;
         const currentPlace = data.location;
-        console.log(currentWeather.temp_c);
-        alert(
-          `the weather in ${event.target.innerText} is ${currentWeather.temp_c} degrees`
-        );
+        getWeatherWindow(event, city, currentWeather, currentPlace)
       } catch (e) {
-        alert(
-          "city is not supported, please write nearby city name in english to get the weather"
-        );
+        unableToGetWeather(event, city);
+        // alert(
+        //   "city is not supported, please write nearby city name in english to get the weather"
+        // );
+      } finally {
+        city.addEventListener('mouseleave', () => {
+          if (document.querySelector('.weather_window')) {
+            const removeWeatherWindow = document.querySelector('.weather_window')
+            removeWeatherWindow.parentElement.removeChild(removeWeatherWindow);
+          }
+        })
       }
     });
   });
